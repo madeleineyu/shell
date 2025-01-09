923dff1f022b5407c70378bc891a6d1311943a2c
madeleineyu <madeleine.yu@mail.utoronto.ca>
2025-01-09 16:13:33 -0500

    Second commit, finished assignment

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -32,14 +32,20 @@ mkdir processed
 cd processed
 mkdir server_logs user_logs event_logs
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
-
+cd ~/DSI_assignments/data
+cp raw/server*.log ~/DSI_assignments/data/processed/server_logs
 # 6. Repeat the above step for user logs and event logs
-
+cd ~/DSI_assignments/data
+cp raw/user*.log ~/DSI_assignments/data/processed/user_logs
+cd ~/DSI_assignments/data
+cp raw/event*.log ~/DSI_assignments/data/processed/event_logs
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-
+rm -i raw/*ipaddr*
+rm -i processed/user_logs/*ipaddr*
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-
-
+touch inventory.txt
+code inventory.txt
+bash inventory.sh
 
 ###########################################
 
